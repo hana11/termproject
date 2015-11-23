b458e91bda4fcf82c27a8c6aa660e7deb60d6365
hana <hana5638@gmail.com>
2015-11-23 00:31:29 -0800

    test2

--- makeSudoku.c
+++ makeSudoku.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+char buf[90];
+int sudoku[9][9]={0,};
+void selectSudoku()
+{
+	int i,j,k=1;
+	 for(i=0; i<9; i++)
+        {
+                for(j=0; j<9; j++)
+                {
+                        k++;
+                        sudoku[i][j]=buf[k];
+                        printf("%c",sudoku[i][j]);
+                }
+                printf("\n");
+        }
+
+}
 int main(void)
 {
-	int sudoku[9][9]={0,};
-	char buf[90];
 	FILE *fp;
 	int i,j,k=1;
-	fp = fopen("ex1.txt","r");
-	if(fp == NULL)
+	int ans =0;
+ 	while(1)
 	{
-		printf("FILE OPEN ERROR\n");
-		exit(0);
-	}
-
-	fgets(buf,sizeof(buf),fp);
-
-	fclose(fp);
-
-
-	for(i=0; i<9; i++)
-	{
-		for(j=0; j<9; j++)
+		printf("Select Sudoku- Easy : 1,2 Medium : 3,4 Hard : 5 , 6 Stop : 999 \n");
+        	scanf("%d",&ans);
+		if(ans == 999)
 		{
-			k++;
-			sudoku[i][j]=buf[k];
-			printf("%c",sudoku[i][j]);
+			break;
 		}
-		printf("\n");
+		fp = fopen("ex1.txt","r");
+		if(fp == NULL)
+		{
+			printf("FILE OPEN ERROR\n");
+			exit(0);
+		}
+		for(i=0; i<ans; i++)
+		{
+			fgets(buf,sizeof(buf),fp);
+		}
+		fclose(fp);
+		selectSudoku();
 	}
 
 }
