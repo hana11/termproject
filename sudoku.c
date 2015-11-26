8be5ac6c6c0cc1b6f9b97cb957c049f539c0e12a
hana <hana5638@gmail.com>
2015-11-25 22:02:48 -0800

    ver2

--- sudoku.c
+++ sudoku.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 int sudoku[10][10];
+void showSudoku()
+{
+	int q_sudoku[9][9]={0,};
+	char buf[90];
+        FILE *fp;
+        int i,j,k=1;
+        fp = fopen("ex1.txt","r");
+        if(fp == NULL)
+        {
+                perror("FILE OPEN ERROR\n");
+                exit(0);
+        }
+
+        fgets(buf,sizeof(buf),fp);
+        fclose(fp);
+
+	for(i=0; i<9; i++)
+        {
+                for(j=0; j<9; j++)
+                {
+                        k++;
+                       	q_sudoku[i][j]=buf[k];
+                        printf("%c",q_sudoku[i][j]);
+                }
+                printf("\n");
+        }
+}
 bool check_sudoku(int x, int y, int value) //coordinate, value
 {
 	int i, j;
@@ -11,22 +40,21 @@ bool check_sudoku(int x, int y, int value) //coordinate, value
 	}
 	tx = (x-1)/3;
 	ty = (y-1)/3;
-	
+
 	for(i=tx*3; i<tx*3+3; i++) {
 		for(j=ty*3; j<ty*3+3; j++) {
 			if(sudoku[i][j]==value)
-				return false;		
+				return false;
 		}
-	} 
+	}
 	return true;
 }
 int main()
 {
 	int x, y,fd;
- 
-
-	while()
+	while(1)
 	{
+		showsudoku();
 		if(check_sudoku(x, y, value))
 		{
 			sudoku[x][y]=value;
