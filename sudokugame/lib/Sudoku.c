f7d8b8812afa5fc68cb0f3eea8aae1db13555094
byeoneon <kp8506@naver.com>
2015-12-08 03:22:24 -0800

    final ver

--- sudokugame/lib/Sudoku.c
+++ sudokugame/lib/Sudoku.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 
-int sudoku[10][10]={0,}, t_sudoku[10][10], ans, life;
+int sudoku[10][10]={0,}, t_sudoku[10][10], ans, life, zero_cnt;
 void show_sudoku();
 void get_sudoku();
 bool check_sudoku(int x, int y, int value);
@@ -18,7 +18,7 @@ typedef struct _log
 log input_log[81];
 void get_sudoku(int _level)
 {
-	int i, j, k=1, zero=0;
+	int i, j, k=1;
 	char buf[90];
 	FILE *fp;
 
@@ -47,12 +47,12 @@ void get_sudoku(int _level)
 			t_sudoku[i][j]=sudoku[i][j];
 			if(t_sudoku[i][j]==0)
 			{
-				zero++;
+				zero_cnt++;
 			}
 		}
 	}
 	fclose(fp);
-	life = zero-(_level+1)*7;
+	life = zero_cnt-(_level+1)*7;
 	printf("------ You choose '%d' Question ------\n",_level);
 	show_sudoku();
 }
@@ -67,19 +67,20 @@ void show_sudoku()
 		}
 		printf("\n");
 	}
+	printf("remain blank : %d\n", zero_cnt); 
 	printf("remain life : %d\n", life); 
 }
 bool check_sudoku(int x, int y, int value) //coordinate, value
 {
 	int i, j;
 	int tx, ty;
-	for(i=1; i<=9; i++)
+	for(i=0; i<9; i++)
 	{
 		if(sudoku[x][i]==value || sudoku[i][y]==value)
 			return false;
 	}
-	tx = (x-1)/3;
-	ty = (y-1)/3;
+	tx = x/3;
+	ty = y/3;
 
 	for(i=tx*3; i<tx*3+3; i++) {
 		for(j=ty*3; j<ty*3+3; j++) {
@@ -95,7 +96,7 @@ void undo_sudoku(int undo_x, int undo_y)
 }
 void start_sudoku(int level)
 {
-	int x, y, value, i, j, input_cnt, zero_cnt;
+	int x, y, value, i, j, input_cnt;
 	bool ch;
 
 	input_cnt=0;
@@ -103,31 +104,32 @@ void start_sudoku(int level)
 
 	get_sudoku(level);
 
-	for(i=0; i<9; i++){
-		for(j=0; j<9; j++){
-			if(sudoku[i][j]==0)
-				zero_cnt++;
-		}
-	}
 	while(1)
 	{
+		if(zero_cnt<1 || life==0)
+		{	
+			printf("End...\n");
+			return;
+		}
+		
 		printf("if value is -1, undo 1 step and  99 is giveup\n");
 		printf("enter values (x, y, value) :");
 		scanf("%d %d %d", &x, &y, &value);
+		getchar();
 		ch=0;
-		if(input_cnt==zero_cnt)
-		{	
-			break;
-		}
 		if(value<0)
 		{
 			if(input_cnt>0)
-				undo_sudoku(input_log[--input_cnt].x, input_log[input_cnt].y);
+			{
+				printf("Undo 1 step\n");
+				undo_sudoku(input_log[input_cnt].x, input_log[input_cnt--].y);
+				zero_cnt++;
+			}
 			else
 				printf("cannot undo. re-enter values\n");
 			show_sudoku();
 		}
-		if(value == 99)
+		else if(value == 99)
 		{
 			printf("You give up the game.\n");
 			return ;
@@ -139,7 +141,7 @@ void start_sudoku(int level)
 			else
 			{
 				for(i=0; i<9; i++) {
-					if(check_sudoku(x, y, i))
+					if(check_sudoku(x, y, i+1))
 					{
 						ch=1;
 					}
@@ -149,22 +151,21 @@ void start_sudoku(int level)
 					if(check_sudoku(x, y, value))
 					{
 						sudoku[x][y]=value;
+						zero_cnt--;						
 						show_sudoku();
-						input_log[input_cnt].x = x;
-						input_log[input_cnt++].y = y;
+						input_log[++input_cnt].x = x;
+						input_log[input_cnt].y = y;
+						
 					}
 					else
 					{
-						printf("wrong number\n");
+						printf("wrong number!!\n");
 						life--;
 					}
-					printf("remain life : %d\n", life);
 				}
 				else
 				{
 					printf("There is no value that satisfies the condition.\n");
-					undo_sudoku(input_log[--input_cnt].x, input_log[input_cnt].y);
-					printf("Undo 1 step\n");
 					show_sudoku();
 				}
 			}
